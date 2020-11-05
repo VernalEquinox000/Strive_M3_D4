17016d447092a14b2c8976234213fd03003bfc0f
VernalEquinox <pieroapruzzese@gmail.com>
2020-11-06 00:39:03 +0100

    ex 6 missing

--- index.js
+++ index.js
@@ -15,13 +15,14 @@ const loadBooks = function() {
                 
             
                 let book = document.createElement("div") 
-                book.className = "col-6 col-sm-6 col-md-3 col-lg-2"; 
+                book.className = "col-6 col-sm-6 col-md-3 col-lg-2 book"; 
                 book.innerHTML = `<div class="card" style"min-width:300px">
                                     <img src="${element.img}" class="card-img-top" alt="${element.title}" style="max-width:100%">
                                     <div class="card-body">
                                         <h5 class="card-title">${element.title}</h5>
                                         <p class="card-text">${element.price} €</p>
-                                        <button type="button" class="btn btn-primary" onclick=addToCart()>Add To Cart</button><br>
+                                        <button type="button" class="btn btn-primary" onclick=selectBook()>Add To Cart</button><br>
+                                        <button type="button" class="btn btn-outline-primary" style="display:none" onclick=removeFromList()>Remove From Cart</button><br>
                                         <button type="button" class="btn btn-secondary" onclick=deleteFromPage()>Skip</button>
                                     </div>
                                 </div>
@@ -37,23 +38,67 @@ const loadBooks = function() {
   }
               
               
+const selectBook = function () {
+    let allCols = document.querySelectorAll(".book")
+    let atcButtons = document.querySelectorAll(".btn.btn-primary")
+    let rfcButtons = document.querySelectorAll(".btn.btn-outline-primary")
+    for (let i = 0; i < atcButtons.length; i++) {
+        atcButtons[i].onclick = function () {
+            allCols[i].style.border = "5px solid black"
+            rfcButtons[i].style.display="block"
+            
+            
+            let toCart = allCols[i].cloneNode(true);
+            document.getElementById("cart").appendChild(toCart)
+            
+        }
 
-
+        /* document.querySelectorAll("#cart .btn-primary").display="none" */
+    }
+}
 
 
 const deleteFromPage = function () {
-    let allCards = document.querySelectorAll(".col-6")
+    let allCols = document.querySelectorAll(".col-6")
     let skipButtons = document.querySelectorAll(".btn.btn-secondary")
     for (let i = 0; i < skipButtons.length; i++) {
         skipButtons[i].onclick = function(){
-            allCards[i].style.display="none";
+            allCols[i].style.display="none";
         }    
     }
 
 }
 
+const removeFromList = function () {
+    let colsCart = document.querySelectorAll("#cart .col-6")
+    let rfcButtons = document.querySelectorAll("#cart .btn.btn-outline-primary")
+    for (let i = 0; i < rfcButtons.length; i++) {
+        /* colsCart[i].onclick = function () { style.border = "5px solid red" }
+        rfcButtons[i].style.display = "block" */
+        rfcButtons[i].onclick = function () {
+            colsCart[i].style.display = "none"
+
+        }
+    }
+}
+
+
+const search = function () {
+    let bookTitles = document.querySelectorAll("h5")
+    let titles = [];
+    for (let i = 0; i < bookTitles.length; i++) {
+        titles[i]=bookTitles[i].innerText
+        
+    }
+        
+    console.log(titles)
+}
+
+
+
+
+
 window.onload = function(){
   loadBooks()
 }
 
-          

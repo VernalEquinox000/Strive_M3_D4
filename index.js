9e6be58b909bf2e73ab45fb70095b3414309f8a2
VernalEquinox <pieroapruzzese@gmail.com>
2020-11-05 17:35:40 +0100

    ex 4-5 done

--- index.js
+++ index.js
@@ -16,12 +16,13 @@ const loadBooks = function() {
             
                 let book = document.createElement("div") 
                 book.className = "col-6 col-sm-6 col-md-3 col-lg-2"; 
-                book.innerHTML = `<div class="card">
+                book.innerHTML = `<div class="card" style"min-width:300px">
                                     <img src="${element.img}" class="card-img-top" alt="${element.title}" style="max-width:100%">
                                     <div class="card-body">
                                         <h5 class="card-title">${element.title}</h5>
                                         <p class="card-text">${element.price} €</p>
-                                        <button type="button" class="btn btn-primary" onclick=addToCart()>Add To Cart</button>
+                                        <button type="button" class="btn btn-primary" onclick=addToCart()>Add To Cart</button><br>
+                                        <button type="button" class="btn btn-secondary" onclick=deleteFromPage()>Skip</button>
                                     </div>
                                 </div>
                             </div>`
@@ -31,11 +32,25 @@ const loadBooks = function() {
             }
         )
         .catch(err => {
-              console.error('!!!!!!!!!!!!!!!!!!!! ', err);
+              console.error('try again!', err);
           });
   }
               
               
+
+
+
+
+const deleteFromPage = function () {
+    let allCards = document.querySelectorAll(".col-6")
+    let skipButtons = document.querySelectorAll(".btn.btn-secondary")
+    for (let i = 0; i < skipButtons.length; i++) {
+        skipButtons[i].onclick = function(){
+            allCards[i].style.display="none";
+        }    
+    }
+
+}
 
 window.onload = function(){
   loadBooks()

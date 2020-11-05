02af86b990f0184aa22a1dfc814bdee9da416558
VernalEquinox <pieroapruzzese@gmail.com>
2020-11-05 16:57:59 +0100

    commit2

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ const loadBooks = function() {
             apiResponse.forEach(element => {
                 
             
-                let child = document.createElement("div") 
-                child.className = "col-6 col-sm-2 col-md-3 col-lg-4"; 
-                child.innerHTML = `<div class="card">
+                let book = document.createElement("div") 
+                book.className = "col-6 col-sm-2 col-md-3 col-lg-4"; 
+                book.innerHTML = `<div class="card">
                 <img class="card-img-top" src="${element.img}" style="width:100%; height:200px"/>
                                      <div class="card-body h-100">
                                       <p><strong>${element.title} </strong></br>
@@ -24,11 +24,7 @@ const loadBooks = function() {
                                       <button type="button" class="btn btn-primary" onclick=addToCart()>Add To Cart</button>
                                  </div>
                                 </div>` 
-                child.addEventListener("click", (e) => { 
-                    e.currentTarget.classList.toggle("selected")
-                    selectBooks();
-                })
-            bookList.appendChild(child)
+            bookList.appendChild(book)
             }); 
 
             }
